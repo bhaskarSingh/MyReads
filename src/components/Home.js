b4c43ba0299085f757c7e8efeced4add122d24ce
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-09-23 14:20:50 +0530

    feat: make consistent, and simple baseline for home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from './AppBar/AppBar'
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <CssBaseline>
+        <AppBar />
+    </CssBaseline>
   )
 }
 
