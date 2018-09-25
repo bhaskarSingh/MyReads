85b647e273ee61e14f7cd085a3a298f90af54b0e
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-09-25 14:22:38 +0530

    refactor: AppBar component

---
+++ src/components/AppBar/UserLogin.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+const UserLogin = () => {
+    {auth && (
+        <div>
+          <IconButton
+            aria-owns={open ? 'menu-appbar' : null}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+          </Menu>
+        </div>
+      )}
+}
+
+export default UserLogin;
