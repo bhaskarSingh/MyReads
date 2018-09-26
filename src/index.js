d13327d75fb5ada7ddf013e16585b87f3dc75347
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-09-26 23:53:50 +0530

    feat: add redux dev tool

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import Routes from './routes';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
-import reducers from './reducers'
-const storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
-
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// const storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(promiseMiddleware),
+    // other store enhancers if any
+  ));
 ReactDOM.render(
-    <Provider store={storeWithMiddleware(reducers)} >
+    <Provider store={store} >
         <BrowserRouter>
             <Routes />
         </BrowserRouter>
